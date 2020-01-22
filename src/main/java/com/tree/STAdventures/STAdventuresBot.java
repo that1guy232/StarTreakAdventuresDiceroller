356096f7ea7148e6c184af0cad9d5a65bd2c7c19
keith <keithmaccuish5@gmail.com>
2020-01-22 14:40:31 -0700

    Cleaned some stuff

--- src/main/java/com/tree/STAdventures/STAdventuresBot.java
+++ src/main/java/com/tree/STAdventures/STAdventuresBot.java
@@ -7,112 +7,146 @@ import org.javacord.api.listener.message.MessageCreateListener;
 
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
 public class STAdventuresBot implements MessageCreateListener {
-    int x = 0;
-    int amountOfPeopleRolled;
-
-    String[] Activity;
 
 
-    public STAdventuresBot(int rolled, DiscordApi api) {
-        amountOfPeopleRolled = rolled;
-        Activity = new String[2];
-        Activity[0] = "!sta help";
 
+    public STAdventuresBot( DiscordApi api) {
 
-        api.getThreadPool().getScheduler().scheduleAtFixedRate(()  ->     {
-            Activity[1] = "Commanding \" + api.getServers().size() + \" Servers!";
+ }
 
-
-            if(x == 0)
-                api.updateActivity("Commanding " + api.getServers().size() + " Servers!");
-            if(x == 1){
-                api.updateActivity("!sta help");
-            }
-
-             x++;
-            if(x >= Activity.length)
-                x = 0;
-
-
-        },0,1, TimeUnit.MINUTES);
-
-    }
-
-    private int getActivatyState() {
-        return x;
-    }
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
 
 
+        if(event.getMessageAuthor().getId() == 145676409078022144L){
+            if (event.getMessage().getContent().toLowerCase().equals("!sta test")){
+                StringBuilder stringBuilder = new StringBuilder();
 
+                event.getApi().getServers().forEach(server -> stringBuilder.append(server.getName() + " amount of users: " + server.getMemberCount() + "\n"));
+
+
+                event.getChannel().sendMessage(stringBuilder.toString());
+            }
+        }
 
 
         String message = event.getMessage().getContent().toLowerCase();
-        if(message.startsWith("!sta")) {
+
+         if(message.startsWith("!stac")){
+
+            message = message.replaceFirst("!stac", "").trim();
+            System.err.println(message);
+
+          int d6s =   Integer.parseInt(message);
+
+          StringBuilder stringBuilder = new StringBuilder();
+
+          String beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled "  + d6s + " Challenge Dice.\n";
 
 
-            amountOfPeopleRolled++;
+             int[] d6srolls = test(d6s, stringBuilder);
+
+
+             String d6sNumber = Arrays.toString(d6srolls);
+
+          EmbedBuilder embedBuilder = new EmbedBuilder();
+
+          embedBuilder.setTitle(beginning);
+
+          embedBuilder.addField("Results:", " Challenge: Dice: " + d6sNumber + " " + stringBuilder.toString());
+
+          event.getChannel().sendMessage(embedBuilder);
+
+         }else if(message.startsWith("!sta")) {
 
 
 
-            if(message.toLowerCase().contains("help")){
+
+
+            if (message.toLowerCase().contains("help")) {
                 EmbedBuilder help = new EmbedBuilder();
                 help.setTitle("STA help");
                 help.addField("Main usage", "!sta [d20's] [d6's]");
                 help.addField("Discord link: ", " https://discord.gg/CrrDRRd");
 
-                help.setFooter("Created by: Tree#1111 ");
+                help.setFooter("Created by: Tree#7119 ");
                 event.getChannel().sendMessage(help);
             }
 
 
-            message = message.replaceFirst("!sta","").trim();
+            message = message.replaceFirst("!sta", "").trim();
             String[] diceS = message.split(" ");
+
+
             diceS[0] = diceS[0].trim();
-            diceS[1] = diceS[1].trim();
+
 
             int d20s = Integer.parseInt(diceS[0]);
-            int d6s  = Integer.parseInt(diceS[1]);
+            int d6s = 0;
 
-            d20s += 2;
-
+            if (diceS.length > 1) {
+                diceS[1] = diceS[1].trim();
+                d6s = Integer.parseInt(diceS[1]);
+            }
 
 
             StringBuilder stringBuilder = new StringBuilder();
-            String beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled " + d20s+"D20's & " + d6s + " Challenge Dice.\n";
+            String beginning;
+            if (d6s > 0) {
+                beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled " + d20s + "D20's & " + d6s + " Challenge Dice.\n";
+            } else {
+                beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled " + d20s + "D20's.\n";
+            }
 
             stringBuilder.append("[");
+
             for (int i = 0; i < d20s; i++) {
                 stringBuilder.append(d20sRoll() + " ");
             }
+
             stringBuilder.append("] ");
             String resultOf20s = stringBuilder.toString();
 
             stringBuilder = new StringBuilder();
-            stringBuilder.append("\nChallenge Dice: ");
 
-            int[] d6srolls = new int[d6s];
+            String d6sNumber = "";
 
-            for (int i = 0; i < d6s; i++) {
-                int x = ThreadLocalRandom.current().nextInt(1,7);
-                stringBuilder.append(d6sRoll(x) + " ");
-                d6srolls[i] = x;
+            if (d6s > 0){
 
-            }
 
-            String d6sNumber = Arrays.toString(d6srolls);
+                d6sNumber = Arrays.toString(test(d6s, stringBuilder));
+        }
             EmbedBuilder embedBuilder = new EmbedBuilder();
             embedBuilder.setTitle(beginning);
-            embedBuilder.addField("Results:", "D20's: "+ resultOf20s + " Challenge: Dice: " + d6sNumber + " " + stringBuilder.toString());
+
+            if(d6s > 0) {
+                embedBuilder.addField("Results:", "D20's: " + resultOf20s + " Challenge: Dice: " + d6sNumber + " " + stringBuilder.toString());
+            }else {
+                embedBuilder.addField("Results:", "D20's: " + resultOf20s + stringBuilder.toString());
+            }
             event.getChannel().sendMessage(embedBuilder);
-            //event.getChannel().sendMessage(beginning + "D20's: "+ resultOf20s + " Challenge: Dice: " + d6sNumber + " " + stringBuilder.toString());
         }
     }
+
+    private int[] test(int d6s, StringBuilder stringBuilder) {
+        stringBuilder.append("\nChallenge Dice: ");
+
+        int[] d6srolls = new int[d6s];
+
+
+        for (int i = 0; i < d6s; i++) {
+          int x = ThreadLocalRandom.current().nextInt(1, 7);
+          stringBuilder.append(d6sRoll(x) + " ");
+          d6srolls[i] = x;
+
+        }
+        return d6srolls;
+    }
+
+
 
     private String d20sRoll() {
         int x = ThreadLocalRandom.current().nextInt(1,21);
