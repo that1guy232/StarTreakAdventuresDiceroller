d920fb03aaf5aa93eff83b344290b7508aa83a89
keith <keithmaccuish5@gmail.com>
2020-03-22 19:39:42 -0600

    Removed the emojis per Modphius request. Cleaned up code & changed challenge dice.

--- src/main/java/com/tree/STAdventures/STAdventuresBot.java
+++ src/main/java/com/tree/STAdventures/STAdventuresBot.java
@@ -20,17 +20,7 @@ public class STAdventuresBot implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
 
-        //For my use only get the amount guilds & users and prints them (this will break in the the future)
-        if(event.getMessageAuthor().getId() == 145676409078022144L){
-            if (event.getMessage().getContent().toLowerCase().equals("!sta rollD6s")){
-                StringBuilder stringBuilder = new StringBuilder();
 
-                event.getApi().getServers().forEach(server -> stringBuilder.append(server.getName() + " amount of users: " + server.getMemberCount() + "\n"));
-
-
-                event.getChannel().sendMessage(stringBuilder.toString());
-            }
-        }
 
 
         //Converts the message to lowercase so it's easier to work with.
@@ -52,17 +42,17 @@ public class STAdventuresBot implements MessageCreateListener {
             String beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled "  + d6s + " Challenge Dice.\n";
 
             //gets the rolls.
-            int[] d6srolls = rollD6s(d6s, stringBuilder);
+            int[] D6Rolls = rollD6s(d6s);
 
-            //creates a string of all the rolls
-            String d6sNumber = Arrays.toString(d6srolls);
+
 
             //Building the embed to send
             EmbedBuilder embedBuilder = new EmbedBuilder();
 
             embedBuilder.setTitle(beginning);
 
-            embedBuilder.addField("Results:", " Challenge: Dice results: " + d6sNumber + " " + stringBuilder.toString());
+            embedBuilder.addField("Results:", " Challenge Dice rolls: " + Arrays.toString(D6Rolls) + " " + stringBuilder.toString() +
+                    "\n Challenge Dice Results:" + getD6Results(D6Rolls));
 
             //sending the embed to the channel
             event.getChannel().sendMessage(embedBuilder);
@@ -70,136 +60,137 @@ public class STAdventuresBot implements MessageCreateListener {
          }else if(message.startsWith("!sta")) {
 
 
+             //the help command
+             if (message.toLowerCase().contains("help")) {
+                 EmbedBuilder help = new EmbedBuilder();
+                 help.setTitle("STA help");
+                 help.addField("Main usage", "!sta [d20's] [d6's]");
+                 help.addField("You can also do:", "!stac [d6's] to exclusively roll challenge dice or !sta [d20's] to just roll d20's!");
+                 help.addField("Discord link: ", " https://discord.gg/CrrDRRd");
+                 help.addField("Support:", "Please consider supporting the bot. https://www.paypal.me/TreeAB");
+                 help.setFooter("Created by: Tree#7119  This project is not endorsed, sponsored or affiliated with CBS Studios Inc, Modiphius Entertainment or the Star Trek franchise. ");
+                 event.getChannel().sendMessage(help);
+
+             } else {
 
 
-            //the help command
-            if (message.toLowerCase().contains("help")) {
-                EmbedBuilder help = new EmbedBuilder();
-                help.setTitle("STA help");
-                help.addField("Main usage", "!sta [d20's] [d6's]");
-                help.addField("You can also do:", "!stac [d6's] to exclusively roll challenge dice or !sta [d20's] to just roll d20's!");
-                help.addField("Discord link: ", " https://discord.gg/CrrDRRd");
+                 //cleaning the message.
+                 message = message.replaceFirst("!sta", "").trim();
+                 String[] diceS = message.split(" ");
+                 diceS[0] = diceS[0].trim();
 
-                help.setFooter("Created by: Tree#7119 ");
-                event.getChannel().sendMessage(help);
-            }
+                 //getting the d20's
+                 int d20s = Integer.parseInt(diceS[0]);
+                 //set the value of d6s to avoid errors latter
+                 int d6s = 0;
 
-
-            //cleaning the message.
-            message = message.replaceFirst("!sta", "").trim();
-            String[] diceS = message.split(" ");
-            diceS[0] = diceS[0].trim();
-
-            //getting the d20's
-            int d20s = Integer.parseInt(diceS[0]);
-            //set the value of d6s to avoid errors latter
-            int d6s = 0;
-
-            //get the d6's if there are any.
-            if (diceS.length > 1) {
-                diceS[1] = diceS[1].trim();
-                d6s = Integer.parseInt(diceS[1]);
-            }
+                 //get the d6's if there are any.
+                 if (diceS.length > 1) {
+                     diceS[1] = diceS[1].trim();
+                     d6s = Integer.parseInt(diceS[1]);
+                 }
 
 
-            StringBuilder stringBuilder = new StringBuilder();
-            String beginning;
+                 StringBuilder stringBuilder = new StringBuilder();
+                 String beginning;
 
-            //set up the name of the embed depending on weather or not the user rolled any d6's
-            if (d6s > 0) {
-                beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled " + d20s + "D20's & " + d6s + " Challenge Dice.\n";
-            } else {
-                beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled " + d20s + "D20's.\n";
-            }
-
-
-            //start of the d20's
-            stringBuilder.append("[");
+                 //set up the name of the embed depending on weather or not the user rolled any d6's
+                 if (d6s > 0) {
+                     beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled " + d20s + "D20's & " + d6s + " Challenge Dice.\n";
+                 } else {
+                     beginning = event.getMessage().getAuthor().asUser().get().getName() + " Rolled " + d20s + "D20's.\n";
+                 }
 
 
-            //roll the d20's and append the to the string builder.
-            for (int i = 0; i < d20s; i++) {
-                stringBuilder.append(d20sRoll() + " ");
-            }
-
-            //end of the d20's and set it to string for latter use.
-            stringBuilder.append("] ");
-            String resultOf20s = stringBuilder.toString();
-
-            //new
-            stringBuilder = new StringBuilder();
+                 //start of the d20's
+                 stringBuilder.append("[");
 
 
-            String resultOfD6s = "";
+                 //roll the d20's and append the to the string builder.
+                 for (int i = 0; i < d20s; i++) {
+                     stringBuilder.append(d20sRoll() + " ");
+                 }
 
-            //if there are any D6's roll them and set them to a string for latter use.
-            if (d6s > 0){
-                resultOfD6s = Arrays.toString(rollD6s(d6s, stringBuilder));
-            }
+                 //end of the d20's and set it to string for latter use.
+                 stringBuilder.append("] ");
+                 String resultOf20s = stringBuilder.toString();
 
-            //create the embed and set the title.
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setTitle(beginning);
+                 //new
+                 stringBuilder = new StringBuilder();
 
-            //create the result embeied field detrmed by weather or not d6's were rolled.
-            if(d6s > 0) {
-                embedBuilder.addField("Results:", "D20's: " + resultOf20s + " Challenge: Dice results: " + resultOfD6s + " " + stringBuilder.toString());
-            }else {
-                embedBuilder.addField("Results:", "D20's: " + resultOf20s + stringBuilder.toString());
-            }
 
-            //send the message.
-            event.getChannel().sendMessage(embedBuilder);
-        }
+                 int[] D6Rolls = null;
+
+                 //if there are any D6's roll them and set them to a string for latter use.
+                 if (d6s > 0) {
+
+                     D6Rolls = rollD6s(d6s);
+                 }
+
+                 //create the embed and set the title.
+                 EmbedBuilder embedBuilder = new EmbedBuilder();
+                 embedBuilder.setTitle(beginning);
+
+                 //create the result embeied field detrmed by weather or not d6's were rolled.
+                 if (D6Rolls != null) {
+                     embedBuilder.addField("Results:", "D20's: " + resultOf20s +
+                             "\n Challenge Dice rolls: " + Arrays.toString(D6Rolls) + " " + stringBuilder.toString() +
+                             "\n Challenge Dice Results:" + getD6Results(D6Rolls));
+                 } else {
+                     embedBuilder.addField("Results:", "D20's: " + resultOf20s + stringBuilder.toString());
+                 }
+
+                 //send the message.
+                 event.getChannel().sendMessage(embedBuilder);
+             }
+         }
     }
 
-    private int[] rollD6s(int d6s, StringBuilder stringBuilder) {
-        stringBuilder.append("\nChallenge Dice: ");
+    private String getD6Results(int[] d6Rolls) {
+        StringBuilder sb = new StringBuilder();
+        int effects = 0;
+        int total = 0;
+        for (int i = 0; i < d6Rolls.length; i++) {
+            switch (d6Rolls[i]){
+                case 1:
+                    total++;
+                    break;
+                case 2:
+                    total += 2;
+                    break;
+                case 3:
+                case 4:
+                    break;
+                case 5:
+                case 6:
+                    effects++;
+                    total++;
+                    break;
+            }
 
-        int[] d6srolls = new int[d6s];
-
-
-        for (int i = 0; i < d6s; i++) {
-          int x = ThreadLocalRandom.current().nextInt(1, 7);
-          stringBuilder.append(d6sRoll(x) + " ");
-          d6srolls[i] = x;
 
         }
-        return d6srolls;
+        sb.append( " " + total + " Hits,  " + effects + " Effects.");
+        return sb.toString();
+    }
+
+    private int[] rollD6s(int d6s) {
+
+        int[] rolls = new int[d6s];
+
+        for (int i = 0; i < d6s; i++) {
+            rolls[i] = ThreadLocalRandom.current().nextInt(1,7);
+
+        }
+
+
+        return rolls;
     }
 
 
 
     private String d20sRoll() {
-        int x = ThreadLocalRandom.current().nextInt(1,21);
-        if(x == 20){
-            return "<:FWbadge:562544700012363776>\"";
-        }else {
-            return String.valueOf(x);
-        }
+        return String.valueOf(ThreadLocalRandom.current().nextInt(1,21));
+    }
 
-    }
-    /*
-    1 = 1 star
-2 = 2 star
-3 = blank
-4= Blank
-5 = symbol
-6 = symbol
-     */
-    private String d6sRoll(int x) {
-        switch (x){
-            case 1:
-                return "<:1:562544700062695427>";
-            case 2:
-                return "<:2:562544700058370048> ";
-            case 3:
-            case 4:
-                return "<:sblank:562544699861106701>";
-            case 5:
-            case 6:
-                return "<:FWbadge:562544700012363776>";
-        }
-        return "This should not happen contact Tree!";
-    }
 }
